e3072f7ac62c4d35a45681064b3747b9abfe98d4
codekebabs <nitinjha.pro@gmail.com>
2020-10-08 19:45:46 +0530

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NgEventBus } from 'ng-event-bus';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [NgEventBus]
 })
 export class AppModule { }
